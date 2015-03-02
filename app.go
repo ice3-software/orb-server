31cba2f41b504162022d5f2c90fbaaa207ecc84d
stephen fortune <steve.fortune@icecb.com>
2015-03-02 16:51:54 +0000

    Crude yet effective registration process - clients are assigned to a waiting room until its full, at which point they are notified that its full. I need to work on the concurrency design here

--- app.go
+++ app.go
@@ -8,12 +8,105 @@ import (
 )
 
 
-type OrbServer struct {
-	Host		net.IP
-	Port		uint16
-	Conns		chan *net.TCPConn
+type OrbClient struct {
+	X			float32
+	Y 			float32
+	Conn 		*net.TCPConn
+	Room		*Room
 }
 
+
+type Room struct {
+	Clients []*OrbClient
+}
+
+func (self *Room) Join(client *OrbClient) {
+	self.Clients = append(self.Clients, client)
+	if self.Full() {
+		self.notifyFull()
+	}
+}
+
+func (self *Room) Count() int {
+	return len(self.Clients)
+}
+
+func (self *Room) notifyFull() {
+	for _, client := range self.Clients {
+		client.Conn.Write([]byte("Let the games begin!"))
+	}
+}
+
+func (self *Room) Full() bool {
+	return len(self.Clients) > 5
+}
+
+func NewRoom() *Room {
+	return &Room{
+		Clients: make([]*OrbClient, 0),
+	}
+}
+
+
+type World struct{
+	Rooms []*Room
+}
+
+func (self *World) Count() int {
+	return len(self.Rooms)
+}
+
+func (self *World) WaitingRoom() (room *Room) {
+
+	if self.Count() == 0 {
+		room = NewRoom()
+		self.Rooms = append(self.Rooms, room)
+		fmt.Println("First waiting room")
+	} else {
+		fmt.Println("Waiting room... ", self.Count())
+		room = self.Rooms[self.Count() - 1]
+	}
+
+	return
+}
+
+func (self *World) Register(conn *net.TCPConn) *OrbClient {
+
+	fmt.Println("Registering client ", self.Count())
+
+	waitingRoom := self.WaitingRoom()
+	client := &OrbClient{
+		Conn: conn,
+		Room: waitingRoom,
+	}
+	waitingRoom.Join(client)
+
+	if waitingRoom.Full() {
+		fmt.Println("Waiting room if full ! Notifying ", self.Count())
+		self.Rooms = append(self.Rooms, NewRoom())
+	}
+
+	return client
+
+}
+
+func NewWorld() *World {
+	return &World{
+		Rooms: make([]*Room, 0),
+	}
+}
+
+
+type OrbServer struct {
+
+	Host		net.IP
+	Port		uint16
+
+	Conns		chan *net.TCPConn
+
+	World		*World
+
+}
 
 func (self *OrbServer) BasePath() string {
 	return fmt.Sprintf("%s:%d", self.Host.String(), self.Port)
@@ -47,23 +140,25 @@ func (self *OrbServer) Listen() error {
 
 func (self *OrbServer) Serve() {
 
+	self.World = NewWorld()
+
 	for {
 
 		select {
-
 		case conn := <-self.Conns:
 
-			conn.Write([]byte("Welcome to orb!\n"))
-			msgBuff := make([]byte, 1024)
-			_, readErr := conn.Read(msgBuff)
+			fmt.Println("New connection")
+			conn.Write([]byte("Welcome to orb! You have been registered in the world\n"))
+			self.World.Register(conn)
 
-			if readErr == nil {
-				conn.Write(msgBuff)
-			} else {
-				conn.Write([]byte(readErr.Error()))
-			}
+			//msgBuff := make([]byte, 1024)
+			//_, readErr := conn.Read(msgBuff)
 
-			conn.Close()
+			//if readErr == nil {
+			//	conn.Write(msgBuff)
+			//} else {
+			//	conn.Write([]byte(readErr.Error()))
+			//}
 
 		break
 
