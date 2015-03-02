162e4a2be0aef84a246a2d0ede2e90c1f7a8d089
stephen fortune <steve.fortune@icecb.com>
2015-03-02 16:01:58 +0000

    Made server concurrent - now handles connections in a separate goroutine.

--- app.go
+++ app.go
@@ -11,6 +11,7 @@ import (
 type OrbServer struct {
 	Host		net.IP
 	Port		uint16
+	Conns		chan *net.TCPConn
 }
 
 
@@ -18,7 +19,7 @@ func (self *OrbServer) BasePath() string {
 	return fmt.Sprintf("%s:%d", self.Host.String(), self.Port)
 }
 
-func (self *OrbServer) Serve() error {
+func (self *OrbServer) Listen() error {
 
 	fmt.Println("Listening to ", self.BasePath())
 	server, err := net.Listen("tcp", self.BasePath())
@@ -27,26 +28,16 @@ func (self *OrbServer) Serve() error {
 		return err
 	}
 
+	go self.Serve()
+
 	for {
 
 		conn, err := server.Accept()
 		if err != nil {
 			fmt.Println("Error accepting incoming connection: ", err)
-			continue
-		}
-
-		conn.Write([]byte("Welcome to orb!\n"))
-
-		msgBuff := make([]byte, 1024)
-		_, readErr := conn.Read(msgBuff)
-
-		if readErr == nil {
-			conn.Write(msgBuff)
 		} else {
-			conn.Write([]byte(readErr.Error()))
+			self.Conns <- conn.(*net.TCPConn)
 		}
-
-		conn.Close()
 
 	}
 
@@ -54,17 +45,44 @@ func (self *OrbServer) Serve() error {
 	return nil
 }
 
+func (self *OrbServer) Serve() {
+
+	for {
+
+		select {
+
+		case conn := <-self.Conns:
+
+			conn.Write([]byte("Welcome to orb!\n"))
+			msgBuff := make([]byte, 1024)
+			_, readErr := conn.Read(msgBuff)
+
+			if readErr == nil {
+				conn.Write(msgBuff)
+			} else {
+				conn.Write([]byte(readErr.Error()))
+			}
+
+			conn.Close()
+
+		break
+
+		}
+	}
+}
+
 func NewOrbServer() *OrbServer {
 	return &OrbServer{
 		Host: net.IPv4(127, 0, 0, 1),
 		Port: 9090,
+		Conns: make(chan *net.TCPConn),
 	}
 }
 
 func main() {
 
 	server := NewOrbServer()
-	err := server.Serve()
+	err := server.Listen()
 
 	if err != nil {
 		fmt.Println("Could not serve")
